54484b0f15faac20bf2f392c3a09b955b4923f58
bschwal <bschwal@sandia.gov>
2023-08-16 14:06:43 -0600

    Changing pandoc_py

--- pandoc_man_2_rst.py
+++ pandoc_man_2_rst.py
@@ -1,6 +1,6 @@
 #/usr/bin/python3
 #requires pandoc v3 for man input format
-#point OVIS_ROOT to the build directory
+#point X_ROOT to the build directory
 
 import os
 import glob
@@ -11,5 +11,16 @@ for d in ['man','src/contrib/sampler/*','src/contrib/store/*','src/ldmsd/test/',
     for i in files:
         fname = i.split('/')[-1].replace('.man','.rst')
         #print(f'/usr/local/bin/pandoc -f man -s -t rst --toc {i} -o man2rst/{fname}')
-        os.system('mkdir -p man2rst')
-        os.system(f'/usr/local/bin/pandoc -f man -s -t rst --toc {i} -o man2rst/{fname}')
+        os.system('mkdir -p man2rst/ldms')
+        os.system(f'/usr/local/bin/pandoc -f man -s -t rst --toc {i} -o man2rst/ldms/{fname}')
+
+SOS_ROOT='/opt/ovis/build/sos'
+for d in ['rpc','sos/python']:
+    files = glob.glob(f'{SOS_ROOT}/{d}/*man')
+    for i in files:
+        fname = i.split('/')[-1].replace('.man','.rst')
+        #print(f'/usr/local/bin/pandoc -f man -s -t rst --toc {i} -o man2rst/{fname}')
+        os.system('mkdir -p man2rst/sos')
+        os.system(f'/usr/local/bin/pandoc -f man -s -t rst --toc {i} -o man2rst/sos/{fname}')
+
+#MAP RST TO HIERARCHY CODE HERE

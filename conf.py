d73b63ee9884947afed1b968e57d8938b8a333f2
Sara W <60634446+Snell1224@users.noreply.github.com>
2025-04-08 14:40:41 -0600

    Update conf.py

--- conf.py
+++ conf.py
@@ -283,6 +283,7 @@ theme_logo_icon = "images/ovis-logo.png"
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ["_static"]
+html_extra_path = ["files"]
 html_show_sourcelink = True
 html_sidebars = {
     "**": ["logo-text.html", "globaltoc.html", "localtoc.html", "searchbox.html"]
